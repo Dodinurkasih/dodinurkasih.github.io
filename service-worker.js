6c11f678fb83c5f7df50c093446e4d270aa0426c
Dodinurkasih <dodinurkasih@gmail.com>
2018-11-09 17:58:41 +0700

    ss

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3e50c17b1cc085808c01d0d131c0dbe4.js"
+  "/precache-manifest.ccb9213942a30f6309c10638f9bd11e3.js"
 );
 
 workbox.clientsClaim();
